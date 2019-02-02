f4f73d3f37621e8acaf7672f8678d7b2ef0794d9
Brahim Hadriche <brahim.hadriche@gmail.com>
2019-02-02 12:55:08 -0500

    Create a video server, and support youtube urls in the video player.

---
+++ Assets/YoutubePlayer/YoutubeDownloadProgress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace YoutubePlayer
+{
+    [RequireComponent(typeof(Image))]
+    public class YoutubeDownloadProgress : MonoBehaviour
+    {
+        public YoutubePlayer youtubePlayer;
+
+        private Image bufferProgress;
+        private string videoId;
+        private void Start()
+        {
+            bufferProgress = GetComponent<Image>();
+            if (bufferProgress.sprite == null)
+            {
+                var texture = Texture2D.whiteTexture;
+                var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 100);
+                bufferProgress.sprite = sprite;
+            }
+
+            //A little hacky, but enough for the sake of the example.
+            var url = youtubePlayer.GetComponent<VideoPlayer>().url;
+            videoId = url.Substring(url.LastIndexOf("v=", StringComparison.InvariantCultureIgnoreCase) + 2);
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            bufferProgress.fillAmount = youtubePlayer.GetDownloadProgressForVideo(videoId);
+        }
+    }
+}
