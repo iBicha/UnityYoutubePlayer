612a28579b078e911c5534adac45a05901b1b8d5
Brahim Hadriche <brahim.hadriche@gmail.com>
2019-02-21 19:33:43 -0500

    Added a 360 video example

---
+++ Assets/YoutubePlayer/Scripts/PanoramicSkyboxVideo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace YoutubePlayer
+{
+    [RequireComponent(typeof(VideoPlayer))]
+    public class PanoramicSkyboxVideo : MonoBehaviour
+    {
+        public Material skyboxMaterial;
+
+        public string videoTextureName;
+
+        private VideoPlayer videoPlayer;
+        void Awake()
+        {
+            videoPlayer = GetComponent<VideoPlayer>();
+            videoPlayer.prepareCompleted += VideoPlayerOnPrepareCompleted;
+        }
+
+        private void VideoPlayerOnPrepareCompleted(VideoPlayer source)
+        {
+            if (skyboxMaterial.HasProperty(videoTextureName))
+            {
+                skyboxMaterial.SetTexture(videoTextureName, videoPlayer.texture);
+            }
+            else
+            {
+                skyboxMaterial.mainTexture = videoPlayer.texture;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            videoPlayer.prepareCompleted -= VideoPlayerOnPrepareCompleted;
+        }
+    }
+}
