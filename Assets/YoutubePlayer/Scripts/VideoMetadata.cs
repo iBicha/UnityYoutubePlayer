a46b210f8c7a9c3eb48a06be5a1c726f2995f436
Brahim Hadriche <brahim.hadriche@unity3d.com>
2021-01-10 19:30:48 -0500

    Metadata example

---
+++ Assets/YoutubePlayer/Scripts/VideoMetadata.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using UnityEngine;
+
+namespace YoutubePlayer
+{
+    public class VideoMetadata : MonoBehaviour
+    {
+        // This an example on how to extend the YoutubeVideoMetaData class to get more details about a video.
+        // Information about the video is provided by youtube-dl.
+        public class MyYoutubeVideoMetadata : YoutubeVideoMetaData
+        {
+            [JsonProperty("description")]
+            public string Description;
+            
+            [JsonProperty("view_count")]
+            public long ViewCount;
+        }
+
+        public string videoUrl;
+
+        async void Start()
+        {
+            Debug.Log("Loading...");
+            var metadata = await YoutubeDl.GetVideoMetaDataAsync<MyYoutubeVideoMetadata>(videoUrl);
+            Debug.Log($"Video description: {metadata.Description}");
+            Debug.Log($"View count: {metadata.ViewCount}");
+        }
+    }
+}
