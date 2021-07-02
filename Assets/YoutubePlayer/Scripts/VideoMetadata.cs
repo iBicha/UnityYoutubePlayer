4fc411011e626b44a8b57849335eeea9e83e0b64
Brahim Hadriche <brahim.hadriche@unity3d.com>
2021-07-02 16:22:58 -0400

    Metadata example update

--- Assets/YoutubePlayer/Scripts/VideoMetadata.cs
+++ Assets/YoutubePlayer/Scripts/VideoMetadata.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace YoutubePlayer
 {
@@ -22,16 +23,28 @@ namespace YoutubePlayer
 
         public string videoUrl;
 
+        public Text Info;
+
         async void Start()
         {
-            Debug.Log("Loading...");
+            Log("Loading...");
 
             // Optimize by specifying the fields we're interested in, to avoid downloading everything.
-            string[] fields = {"description", "view_count"};
+            string[] fields = {"title", "description", "view_count"};
 
             var metadata = await YoutubeDl.GetVideoMetaDataAsync<MyYoutubeVideoMetadata>(videoUrl, fields);
-            Debug.Log($"Video description: {metadata.Description}");
-            Debug.Log($"View count: {metadata.ViewCount}");
+            Log($"- Video title: {metadata.Title}");
+            Log($"- Video description: {metadata.Description}");
+            Log($"- View count: {metadata.ViewCount}");
+
+            // You can also omit the fields, and the JsonProperty attributes will be used instead.
+            // var metadata = await YoutubeDl.GetVideoMetaDataAsync<MyYoutubeVideoMetadata>(videoUrl);
+        }
+
+        void Log(string message)
+        {
+            Debug.Log(message);
+            Info.text += message + '\n';
         }
     }
 }
