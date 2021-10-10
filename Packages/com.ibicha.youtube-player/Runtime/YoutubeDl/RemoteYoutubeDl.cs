53a080c65ff7d4be8fbfb44a85191e925ec4f703
Brahim Hadriche <brahim.hadriche@unity3d.com>
2021-10-10 10:57:25 -0400

    Move request url building into own function

--- Packages/com.ibicha.youtube-player/Runtime/YoutubeDl/RemoteYoutubeDl.cs
+++ Packages/com.ibicha.youtube-player/Runtime/YoutubeDl/RemoteYoutubeDl.cs
@@ -11,34 +11,7 @@ namespace YoutubePlayer
     {
         public async Task<T> GetVideoMetaDataAsync<T>(string youtubeUrl, YoutubeDlOptions options, IEnumerable<string> schema, CancellationToken cancellationToken = default)
         {
-            var optionFlags = new List<string>();
-            if (!string.IsNullOrWhiteSpace(options.Format))
-            {
-                optionFlags.Add($"-f \"{options.Format}\"");
-            }
-            if (options.UserAgent != null)
-            {
-                optionFlags.Add($"--user-agent \"{options.UserAgent}\"");
-            }
-            if (options.Custom != null)
-            {
-                optionFlags.Add(options.Custom);
-            }
-
-            var requestUrl = $"{YoutubeDl.ServerUrl}/v1/video?url={youtubeUrl}";
-            if (optionFlags.Count > 0)
-            {
-                requestUrl += $"&options={UnityWebRequest.EscapeURL(string.Join(" ", optionFlags))}";
-            }
-
-            if (schema != null)
-            {
-                foreach (var schemaKey in schema)
-                {
-                    requestUrl += $"&schema={schemaKey}";
-                }
-            }
-
+            var requestUrl = BuildRequestUrl(youtubeUrl, options, schema);
             var request = UnityWebRequest.Get(requestUrl);
             var tcs = new TaskCompletionSource<T>();
             request.SendWebRequest().completed += operation =>
@@ -76,6 +49,39 @@ namespace YoutubePlayer
             }, request);
 
             return await tcs.Task;
+        }
+
+        string BuildRequestUrl(string youtubeUrl, YoutubeDlOptions options, IEnumerable<string> schema)
+        {
+            var optionFlags = new List<string>();
+            if (!string.IsNullOrWhiteSpace(options.Format))
+            {
+                optionFlags.Add($"-f \"{options.Format}\"");
+            }
+            if (options.UserAgent != null)
+            {
+                optionFlags.Add($"--user-agent \"{options.UserAgent}\"");
+            }
+            if (options.Custom != null)
+            {
+                optionFlags.Add(options.Custom);
+            }
+
+            var requestUrl = $"{YoutubeDl.ServerUrl}/v1/video?url={youtubeUrl}";
+            if (optionFlags.Count > 0)
+            {
+                requestUrl += $"&options={UnityWebRequest.EscapeURL(string.Join(" ", optionFlags))}";
+            }
+
+            if (schema != null)
+            {
+                foreach (var schemaKey in schema)
+                {
+                    requestUrl += $"&schema={schemaKey}";
+                }
+            }
+
+            return requestUrl;
         }
     }
 }
