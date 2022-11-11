0ad69008bfe988879c1cca2ca10fd47848a5336b
Brahim Hadriche <brahim.hadriche@gmail.com>
2022-11-10 19:27:43 -0500

    Reuse web request class

--- Packages/com.ibicha.youtube-player/Runtime/YoutubeDl/RemoteYoutubeDl.cs
+++ Packages/com.ibicha.youtube-player/Runtime/YoutubeDl/RemoteYoutubeDl.cs
@@ -9,47 +9,11 @@ namespace YoutubePlayer
 {
     class RemoteYoutubeDl : IYoutubeDl
     {
-        public async Task<T> GetVideoMetaDataAsync<T>(string youtubeUrl, YoutubeDlOptions options,
+        public Task<T> GetVideoMetaDataAsync<T>(string youtubeUrl, YoutubeDlOptions options,
             IEnumerable<string> schema, YoutubeDlCli cli, CancellationToken cancellationToken = default)
         {
             var requestUrl = BuildRequestUrl(youtubeUrl, options, schema, cli);
-            var request = UnityWebRequest.Get(requestUrl);
-            var tcs = new TaskCompletionSource<T>();
-            request.SendWebRequest().completed += operation =>
-            {
-#if UNITY_2020_2_OR_NEWER
-                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.DataProcessingError)
-#else
-                if (request.isNetworkError)
-#endif
-                {
-                    tcs.TrySetException(new Exception(request.error));
-                    return;
-                }
-
-                var text = request.downloadHandler.text;
-
-#if UNITY_2020_2_OR_NEWER
-                if (request.result == UnityWebRequest.Result.ProtocolError)
-#else
-                if (request.isHttpError)
-#endif
-                {
-                    tcs.TrySetException(new Exception(request.error + "\nResponseError:" + text));
-                    return;
-                }
-
-                var video = JsonConvert.DeserializeObject<T>(text);
-                tcs.TrySetResult(video);
-            };
-
-            cancellationToken.Register(obj =>
-            {
-                ((UnityWebRequest)obj).Abort();
-                tcs.TrySetCanceled(cancellationToken);
-            }, request);
-
-            return await tcs.Task;
+            return WebRequest.GetAsync<T>(requestUrl, cancellationToken);
         }
 
         string BuildRequestUrl(string youtubeUrl, YoutubeDlOptions options,
